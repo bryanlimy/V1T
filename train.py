2869ca828bacf6264af21fdd1041899b2bd8e458
bryanlimy <bryanlimy@gmail.com>
2022-09-28 18:10:43 +0100

    update

--- train.py
+++ train.py
@@ -161,7 +161,7 @@ def main(args):
     checkpoint = Checkpoint(args, model=model, optimizer=optimizer, min_epochs=10)
     epoch = checkpoint.restore()
 
-    utils.evaluate(args, ds=val_ds, model=model, epoch=0, summary=summary, mode=1)
+    utils.evaluate(args, ds=val_ds, model=model, epoch=epoch, summary=summary, mode=1)
 
     while (epoch := epoch + 1) < args.epochs + 1:
         print(f"\nEpoch {epoch:03d}/{args.epochs:03d}")
