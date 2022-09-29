0433f1ddd07a72cd4ced00b2b777342f10021e9f
bryanlimy <bryanlimy@gmail.com>
2022-09-29 16:53:50 +0100

    update

--- src/sensorium/models/core/stn.py
+++ src/sensorium/models/core/stn.py
@@ -2,10 +2,9 @@ from .core import register, Core
 
 import torch
 import typing as t
+import numpy as np
 from torch import nn
 import torch.nn.functional as F
-
-import numpy as np
 
 from sensorium.models import utils
 
@@ -28,10 +27,10 @@ class SpatialTransformerCore(Core):
         self.localization = nn.Sequential(
             nn.Conv2d(in_channels=1, out_channels=8, kernel_size=7),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.ReLU(inplace=True),
+            nn.ReLU(True),
             nn.Conv2d(in_channels=8, out_channels=10, kernel_size=5),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.ReLU(inplace=True),
+            nn.ReLU(True),
         )
 
         stn_shape = utils.conv2d_output_shape(
@@ -43,30 +42,24 @@ class SpatialTransformerCore(Core):
         )
         stn_shape = (stn_shape[0], stn_shape[1] // 2, stn_shape[2] // 2)
 
+        self.flatten = nn.Flatten()
+
         # Regressor for the 3 * 2 affine matrix
-        self.regressor = nn.Sequential(
-            nn.Flatten(),
+        self.fc_loc = nn.Sequential(
             nn.Linear(in_features=int(np.prod(stn_shape)), out_features=32),
             nn.ReLU(True),
             nn.Linear(in_features=32, out_features=3 * 2),
         )
 
         # Initialize the weights/bias with identity transformation
-        self.regressor[3].weight.data.zero_()
-        self.regressor[3].bias.data.copy_(
+        self.fc_loc[2].weight.data.zero_()
+        self.fc_loc[2].bias.data.copy_(
             torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float32)
         )
 
-        # Feedforward network
-        self.feedforward = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=10, kernel_size=5),
-            nn.MaxPool2d(kernel_size=2),
-            nn.GELU(),
-            nn.Conv2d(in_channels=10, out_channels=20, kernel_size=5),
-            nn.Dropout2d(p=args.dropout),
-            nn.MaxPool2d(kernel_size=2),
-            nn.GELU(),
-        )
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=10, kernel_size=5)
+        self.conv2 = nn.Conv2d(in_channels=10, out_channels=20, kernel_size=5)
+        self.dropout2d = nn.Dropout2d(p=0.25)
 
         output_shape = utils.conv2d_output_shape(
             input_shape=input_shape, num_filters=10, kernel_size=5
@@ -79,15 +72,25 @@ class SpatialTransformerCore(Core):
         self._output_shape = output_shape
 
     # Spatial transformer network forward function
-    def stn(self, inputs: torch.Tensor, align_corners: bool = False):
+    def stn(self, inputs: torch.Tensor, align_corners: bool = True):
         spatial = self.localization(inputs)
-        theta = self.regressor(spatial)
+        spatial = self.flatten(spatial)
+        theta = self.fc_loc(spatial)
         theta = theta.view(-1, 2, 3)
         grid = F.affine_grid(theta, size=inputs.size(), align_corners=align_corners)
         outputs = F.grid_sample(inputs, grid=grid, align_corners=align_corners)
         return outputs
 
     def forward(self, inputs: torch.Tensor):
+        # transform the input
         outputs = self.stn(inputs)
-        outputs = self.feedforward(outputs)
+
+        # Perform the usual forward pass
+        outputs = self.conv1(outputs)
+        outputs = F.max_pool2d(outputs, kernel_size=2)
+        outputs = F.gelu(outputs)
+        outputs = self.conv2(outputs)
+        outputs = self.dropout2d(outputs)
+        outputs = F.max_pool2d(outputs, kernel_size=2)
+        outputs = F.gelu(outputs)
         return outputs
