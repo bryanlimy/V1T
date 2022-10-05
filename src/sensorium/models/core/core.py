fix regularization calculation. add ds_scale to scale loss by the size of the dataset

src/sensorium/models/core/core.py
@@ -1,5 +1,6 @@
 _CORES = dict()
 
+import torch
 import typing as t
 from torch import nn
 
@@ -20,6 +21,10 @@ class Core(nn.Module):
         super(Core, self).__init__()
         self.input_shape = input_shape
         self.name = name
+        self._device = args.device
+        self.reg_scale = torch.tensor(
+            args.reg_scale, dtype=torch.float32, device=self._device
+        )
 
     @property
     def shape(self):
@@ -30,7 +35,7 @@ class Core(nn.Module):
 
     def regularizer(self):
         """L1 regularization"""
-        return sum(p.abs().sum() for p in self.parameters())
+        return self.reg_scale * sum(p.abs().sum() for p in self.parameters())
 
 
 def get_core(args):

src/sensorium/models/model.py
@@ -36,6 +36,7 @@ class Model(nn.Module):
         self.add_module(
             name="readouts",
             module=Readouts(
+                args,
                 model=args.readout,
                 input_shape=self.core.shape,
                 output_shapes=self.output_shapes,
@@ -43,10 +44,8 @@ class Model(nn.Module):
             ),
         )
 
-    def regularizer(self):
-        reg_loss = self.core.regularizer()
-        for mouse_id in self.readouts.keys():
-            reg_loss += self.readouts[mouse_id].regularizer()
+    def regularizer(self, mouse_id: int):
+        reg_loss = self.core.regularizer() + self.readouts.regularizer(mouse_id)
         return reg_loss
 
     def forward(self, inputs: torch.Tensor, mouse_id: torch.Union[int, torch.Tensor]):

src/sensorium/models/readout/gaussian2d.py
@@ -14,6 +14,7 @@ REDUCTIONS = t.Literal["sum", "mean", None]
 class Gaussian2DReadout(Readout):
     def __init__(
         self,
+        args,
         input_shape: tuple,
         output_shape: tuple,
         ds: DataLoader,
@@ -25,7 +26,7 @@ class Gaussian2DReadout(Readout):
         name: str = "Gaussian2DReadout",
     ):
         super(Gaussian2DReadout, self).__init__(
-            input_shape=input_shape, output_shape=output_shape, ds=ds, name=name
+            args, input_shape=input_shape, output_shape=output_shape, ds=ds, name=name
         )
 
         if init_mu_range > 1.0 or init_mu_range <= 0.0 or init_sigma <= 0.0:
@@ -34,6 +35,10 @@ class Gaussian2DReadout(Readout):
                 "init_sigma_range is non-positive"
             )
         self.init_mu_range = init_mu_range
+
+        self.gamma_readout = torch.tensor(
+            0.0076, dtype=torch.float32, device=self._device
+        )
 
         # position grid shape
         self.grid_shape = (1, self.num_neurons, 1, 2)
@@ -88,7 +93,7 @@ class Gaussian2DReadout(Readout):
         return l1
 
     def regularizer(self, reduction: REDUCTIONS = "sum"):
-        return self.feature_l1(reduction=reduction)
+        return self.gamma_readout * self.feature_l1(reduction=reduction)
 
     def init_grid_predictor(
         self,

src/sensorium/models/readout/linear.py
@@ -10,13 +10,14 @@ from torch.utils.data import DataLoader
 class LinearReadout(Readout):
     def __init__(
         self,
+        args,
         input_shape: tuple,
         output_shape: tuple,
         ds: DataLoader,
         name: str = "LinearReadout",
     ):
         super(LinearReadout, self).__init__(
-            input_shape=input_shape, output_shape=output_shape, ds=ds, name=name
+            args, input_shape=input_shape, output_shape=output_shape, ds=ds, name=name
         )
 
         self.linear = nn.Sequential(

src/sensorium/models/readout/readout.py
@@ -22,16 +22,25 @@ class Readout(nn.Module):
     """Basic readout module for a single rodent"""
 
     def __init__(
-        self, input_shape: tuple, output_shape: tuple, ds: DataLoader, name: str = None
+        self,
+        args,
+        input_shape: tuple,
+        output_shape: tuple,
+        ds: DataLoader,
+        name: str = None,
     ):
         super(Readout, self).__init__()
         self.name = "Readout" if name is None else name
         self._input_shape = input_shape
         self._output_shape = output_shape
+        self._device = args.device
         self._response_stat = {
             k: torch.from_numpy(v) for k, v in ds.dataset.stats["response"].items()
         }
         self._neurons_coordinate = torch.from_numpy(ds.dataset.neurons_coordinate)
+        self.reg_scale = torch.tensor(
+            args.reg_scale, dtype=torch.float32, device=self._device
+        )
 
     @property
     def shape(self):
@@ -51,7 +60,8 @@ class Readout(nn.Module):
         pass
 
     def regularizer(self, reduction: str):
-        return 0
+        """L1 regularization"""
+        return self.reg_scale * sum(p.abs().sum() for p in self.parameters())
 
 
 class Readouts(nn.ModuleDict):
@@ -59,6 +69,7 @@ class Readouts(nn.ModuleDict):
 
     def __init__(
         self,
+        args,
         model: str,
         input_shape: t.Tuple[int],
         output_shapes: t.Dict[int, tuple],
@@ -69,11 +80,13 @@ class Readouts(nn.ModuleDict):
             raise NotImplementedError(f"Readout {model} has not been implemented.")
         self._input_shape = input_shape
         self._output_shapes = output_shapes
+        self._device = args.device
         readout_model = _READOUTS[model]
         for mouse_id, output_shape in self._output_shapes.items():
             self.add_module(
                 name=str(mouse_id),
                 module=readout_model(
+                    args,
                     input_shape=input_shape,
                     output_shape=output_shape,
                     ds=ds[mouse_id],
@@ -86,8 +99,8 @@ class Readouts(nn.ModuleDict):
         for mouse_id, readout in self.items():
             readout.initialize()
 
-    def regularizer(self, mouse_id: str = None, reduction: str = "sum"):
-        return self[mouse_id].regularizer(reduction=reduction)
+    def regularizer(self, mouse_id: int, reduction: str = "sum"):
+        return self[str(mouse_id)].regularizer(reduction=reduction)
 
     def forward(self, inputs: torch.Tensor, mouse_id: torch.Union[int, torch.Tensor]):
         return self[str(mouse_id)](inputs)

train.py
@@ -9,7 +9,7 @@ from shutil import rmtree
 from torch.utils.data import DataLoader
 
 from sensorium import losses, metrics
-from sensorium.models import get_model
+from sensorium.models import get_model, Model
 from sensorium.data import get_training_ds
 from sensorium.utils import utils, tensorboard, checkpoint
 
@@ -26,10 +26,9 @@ def compute_metrics(y_true: torch.Tensor, y_pred: torch.Tensor):
 
 def train_step(
     mice_data: t.Tuple[t.Dict[str, torch.Tensor]],
-    model: nn.Module,
+    model: Model,
     optimizer: torch.optim,
     criterion: losses.Loss,
-    reg_scale: t.Union[float, torch.Tensor] = 0,
 ) -> t.Dict[int, t.Dict[str, torch.Tensor]]:
     result = {}
     for data in mice_data:
@@ -38,8 +37,8 @@ def train_step(
         responses = data["response"].to(model.device)
         outputs = model(images, mouse_id=mouse_id)
         loss = criterion(y_true=responses, y_pred=outputs, mouse_id=mouse_id)
-        reg_loss = model.regularizer()
-        total_loss = loss + reg_scale * reg_loss
+        reg_loss = model.regularizer(mouse_id=mouse_id)
+        total_loss = loss + reg_loss
         total_loss.backward()  # calculate and accumulate gradients
         result[mouse_id] = {
             "loss/loss": loss.item(),
@@ -55,7 +54,7 @@ def train_step(
 def train(
     args,
     ds: t.Dict[int, DataLoader],
-    model: nn.Module,
+    model: Model,
     optimizer: torch.optim,
     criterion: losses.Loss,
     epoch: int,
@@ -74,7 +73,6 @@ def train(
                 model=model,
                 optimizer=optimizer,
                 criterion=criterion,
-                reg_scale=args.reg_scale,
             )
             for mouse_id in mouse_ids:
                 utils.update_dict(results[mouse_id], step_results[mouse_id])
@@ -186,7 +184,7 @@ def main(args):
     )
     epoch = ckpt.restore()
 
-    utils.evaluate(args, ds=val_ds, model=model, epoch=epoch, summary=summary, mode=1)
+    # utils.evaluate(args, ds=val_ds, model=model, epoch=epoch, summary=summary, mode=1)
 
     while (epoch := epoch + 1) < args.epochs + 1:
         print(f"\nEpoch {epoch:03d}/{args.epochs:03d}")
@@ -336,6 +334,11 @@ if __name__ == "__main__":
         default=0,
         type=float,
         help="weight regularization coefficient.",
+    )
+    parser.add_argument(
+        "--ds_scale",
+        action="store_true",
+        help="scale loss by the size of the dataset",
     )
     parser.add_argument("--lr", default=1e-4, type=float, help="model learning rate")
     parser.add_argument(