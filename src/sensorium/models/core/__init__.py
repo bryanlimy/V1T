968793649c8f4975104ca40b4d5fad43d373aabd
bryanlimy <bryanlimy@gmail.com>
2022-10-07 13:03:17 +0100

    update

--- src/sensorium/models/core/__init__.py
+++ src/sensorium/models/core/__init__.py
@@ -1,9 +1,9 @@
-__all__ = ["conv", "linear", "stacked2d", "mixer", "stn", "vit"]
+__all__ = ["conv", "get_core", "linear", "stacked2d", "mixer", "stn", "vit"]
 
 from .conv import *
+from .core import get_core
 from .linear import *
 from .stacked2d import *
 from .mixer import *
 from .stn import *
 from .vit import *
-from .core import get_core
