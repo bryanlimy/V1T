9107a8e86d0c9c84d78d63e02ba21f6f32116635
bryanlimy <bryanlimy@gmail.com>
2022-10-13 16:30:35 +0100

    update

--- ensemble.py
+++ ensemble.py
@@ -165,7 +165,7 @@ class EnsembleModel(nn.Module):
             ELU1(),
         )
 
-    def regularizer(self):
+    def regularizer(self, mouse_id: int):
         return 0
 
     def forward(self, inputs: torch.Tensor, mouse_id: int, pupil_center: torch.Tensor):
