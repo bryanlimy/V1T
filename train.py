9a35c2b5bf427a5c5e72cda363aaac26d1d1615d
bryanlimy <bryanlimy@gmail.com>
2022-10-17 18:23:19 +0100

    add shifter to optimizer param group

--- train.py
+++ train.py
@@ -163,18 +163,21 @@ def main(args):
         utils.load_pretrain_core(args, model=model)
 
     # separate learning rates for core and readout modules
+    params = [
+        {
+            "params": model.core.parameters(),
+            "lr": args.core_lr_scale * args.lr,
+            "name": "core",
+        },
+        {
+            "params": model.readouts.parameters(),
+            "name": "readouts",
+        },
+    ]
+    if args.use_shifter:
+        params.append({"params": model.shifter.parameters(), "name": "shifter"})
     optimizer = torch.optim.Adam(
-        params=[
-            {
-                "params": model.core.parameters(),
-                "lr": args.core_lr_scale * args.lr,
-                "name": "core",
-            },
-            {
-                "params": model.readouts.parameters(),
-                "name": "readouts",
-            },
-        ],
+        params=params,
         lr=args.lr,
         betas=(args.adam_beta1, args.adam_beta2),
         eps=args.adam_eps,
