48bb8513302d3521ecb415512903b5758d6edceb
bryanlimy <bryanlimy@gmail.com>
2022-12-12 10:49:49 +0000

    update

--- sweep.py
+++ sweep.py
@@ -8,12 +8,20 @@ import train as trainer
 
 
 class Args:
-    def __init__(self, id: str, config: wandb.Config, dataset: str, batch_size: int):
+    def __init__(
+        self,
+        id: str,
+        config: wandb.Config,
+        dataset: str,
+        batch_size: int,
+        num_workers: int = 2,
+    ):
         self.dataset = dataset
         self.output_dir = os.path.join(
             config.output_dir, f"{datetime.now():%Y%m%d-%Hh%Mm}-{id}"
         )
         self.batch_size = batch_size
+        self.num_workers = num_workers
         self.device = ""
         self.mouse_ids = None
         self.seed = 1234
@@ -28,13 +36,19 @@ class Args:
                 setattr(self, key, value)
 
 
-def main(dataset: str, batch_size: int):
+def main(dataset: str, batch_size: int, num_workers: int = 2):
     run = wandb.init(group="vit_sweep")
     config = run.config
     run.name = run.id
     wandb.config.update({"batch_size": batch_size})
 
-    args = Args(id=run.id, config=config, dataset=dataset, batch_size=batch_size)
+    args = Args(
+        id=run.id,
+        config=config,
+        dataset=dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+    )
     trainer.main(args, wandb_sweep=True)
 
 
@@ -47,6 +61,7 @@ if __name__ == "__main__":
         help="path to directory where the dataset is stored.",
     )
     parser.add_argument("--batch_size", type=int, default=0)
+    parser.add_argument("--num_workers", type=int, default=4)
     parser.add_argument("--sweep_id", type=str, required=True)
     parser.add_argument("--num_trials", type=int, default=1)
     params = parser.parse_args()
@@ -57,6 +72,7 @@ if __name__ == "__main__":
             main,
             dataset=params.dataset,
             batch_size=params.batch_size,
+            num_workers=params.num_workers,
         ),
         count=params.num_trials,
     )
