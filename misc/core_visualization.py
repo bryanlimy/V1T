e31525c7a36502bbaa5b60ddc3a353470eb13256
bryanlimy <bryanlimy@gmail.com>
2022-12-14 19:59:29 +0000

    update

--- misc/core_visualization.py
+++ misc/core_visualization.py
@@ -83,30 +83,33 @@ def main(args):
     scheduler = Scheduler(args, model=model, save_optimizer=False)
     scheduler.restore(force=True)
 
-    # df = pd.DataFrame(columns=["Mouse", "Component 1", "Component 2"])
-    # for mouse_id, mouse_ds in test_ds.items():
-    #     latents = inference(
-    #         mouse_id=mouse_id, ds=mouse_ds, model=model, device=args.device
-    #     )
-    #     latents = np.sum(latents, axis=1)  # sum over channel dimension
-    #     pca = PCA(n_components=2)
-    #     pca.fit(latents)
-    #     print(f"Explained variance ratio: {pca.explained_variance_ratio_}")
-    #     factors = pca.transform(latents)
-    #     mouse_df = pd.DataFrame(
-    #         data={
-    #             "Mouse": np.repeat(mouse_id, repeats=len(factors)),
-    #             "Component 1": factors[..., 0],
-    #             "Component 2": factors[..., 1],
-    #         }
-    #     )
-    #     df = pd.concat([df, mouse_df], ignore_index=True)
+    df = pd.DataFrame(columns=["Mouse", "Component 1", "Component 2"])
+    for mouse_id, mouse_ds in test_ds.items():
+        latents = inference(
+            mouse_id=mouse_id, ds=mouse_ds, model=model, device=args.device
+        )
+        latents = np.sum(latents, axis=1)  # sum over channel dimension
+        pca = PCA(n_components=2)
+        pca.fit(latents)
+        print(f"Explained variance ratio: {pca.explained_variance_ratio_}")
+        factors = pca.transform(latents)
+        mouse_df = pd.DataFrame(
+            data={
+                "Mouse": np.repeat(mouse_id, repeats=len(factors)),
+                "Component 1": factors[..., 0],
+                "Component 2": factors[..., 1],
+            }
+        )
+        df = pd.concat([df, mouse_df], ignore_index=True)
 
     import pickle
 
-    with open("057_dim1.pkl", "rb") as file:
+    with open("070_dim1.pkl", "wb") as file:
+        pickle.dump(df, file)
         df = pickle.load(file)
+    exit()
 
+    tick_fontsize, label_fontsize = 10, 12
     figure, ax = plt.subplots(nrows=1, ncols=1, figsize=(5, 5), dpi=120)
     sns.scatterplot(
         data=df,
@@ -117,7 +120,40 @@ def main(args):
         alpha=0.6,
         ax=ax,
     )
+    x_range = np.linspace(
+        np.ceil(df["Component 1"].min()), np.floor(df["Component 1"].max()), 3
+    )
+    tensorboard.set_xticks(
+        axis=ax,
+        ticks_loc=x_range,
+        ticks=x_range.astype(int),
+        label="Component 1",
+        tick_fontsize=tick_fontsize,
+        label_fontsize=label_fontsize,
+    )
+    y_range = np.linspace(
+        np.ceil(df["Component 2"].min()), np.floor(df["Component 2"].max()), 3
+    )
+    tensorboard.set_yticks(
+        axis=ax,
+        ticks_loc=y_range,
+        ticks=y_range.astype(int),
+        label="Component 2",
+        tick_fontsize=tick_fontsize,
+        label_fontsize=label_fontsize,
+    )
     sns.despine(ax=ax, offset={"left": 15, "bottom": 5}, trim=True)
+    sns.move_legend(
+        ax,
+        loc="best",
+        frameon=True,
+        handletextpad=0.2,
+        markerscale=0.6,
+        fontsize=tick_fontsize,
+        title_fontsize=tick_fontsize,
+    )
+    ax.set_title("Latent factors of ViT core", fontsize=label_fontsize)
+    figure.tight_layout()
     plt.show()
 
 
