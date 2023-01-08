cf56e66ac9b6ec2d95675776727d38520d02153f
Bryan M. Li <bryanlimy@gmail.com>
2023-01-08 11:14:29 +0000

    stacked2d ensemble

--- ensemble.py
+++ ensemble.py
@@ -272,17 +272,29 @@ def main(args):
             args.use_wandb = False
 
     # pretrained model to load
+    # args.saved_models = {
+    #     "1": "runs/vit_ensemble/001_vit_gaussian2d_seed1",
+    #     "2": "runs/vit_ensemble/002_vit_gaussian2d_seed2",
+    #     "3": "runs/vit_ensemble/003_vit_gaussian2d_seed3",
+    #     "4": "runs/vit_ensemble/004_vit_gaussian2d_seed4",
+    #     "5": "runs/vit_ensemble/005_vit_gaussian2d_seed5",
+    #     "6": "runs/vit_ensemble/006_vit_gaussian2d_seed6",
+    #     "7": "runs/vit_ensemble/007_vit_gaussian2d_seed7",
+    #     "8": "runs/vit_ensemble/008_vit_gaussian2d_seed8",
+    #     "9": "runs/vit_ensemble/009_vit_gaussian2d_seed9",
+    #     "10": "runs/vit_ensemble/010_vit_gaussian2d_seed10",
+    # }
     args.saved_models = {
-        "1": "runs/vit_ensemble/001_vit_gaussian2d_seed1",
-        "2": "runs/vit_ensemble/002_vit_gaussian2d_seed2",
-        "3": "runs/vit_ensemble/003_vit_gaussian2d_seed3",
-        "4": "runs/vit_ensemble/004_vit_gaussian2d_seed4",
-        "5": "runs/vit_ensemble/005_vit_gaussian2d_seed5",
-        "6": "runs/vit_ensemble/006_vit_gaussian2d_seed6",
-        "7": "runs/vit_ensemble/007_vit_gaussian2d_seed7",
-        "8": "runs/vit_ensemble/008_vit_gaussian2d_seed8",
-        "9": "runs/vit_ensemble/009_vit_gaussian2d_seed9",
-        "10": "runs/vit_ensemble/010_vit_gaussian2d_seed10",
+        "1": "runs/stacked2d_ensemble/001_stacked2d_gaussian2d_seed1",
+        "2": "runs/stacked2d_ensemble/002_stacked2d_gaussian2d_seed2",
+        "3": "runs/stacked2d_ensemble/003_stacked2d_gaussian2d_seed3",
+        "4": "runs/stacked2d_ensemble/004_stacked2d_gaussian2d_seed4",
+        "5": "runs/stacked2d_ensemble/005_stacked2d_gaussian2d_seed5",
+        "6": "runs/stacked2d_ensemble/006_stacked2d_gaussian2d_seed6",
+        "7": "runs/stacked2d_ensemble/007_stacked2d_gaussian2d_seed7",
+        "8": "runs/stacked2d_ensemble/008_stacked2d_gaussian2d_seed8",
+        "9": "runs/stacked2d_ensemble/009_stacked2d_gaussian2d_seed9",
+        "10": "runs/stacked2d_ensemble/010_stacked2d_gaussian2d_seed10",
     }
 
     model = EnsembleModel(args, saved_models=args.saved_models, ds=train_ds)
