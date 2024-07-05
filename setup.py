5fc01fd29aae7a883e9e2d1e7c639e722436a9a9
Bryan M. Li <bryanlimy@gmail.com>
2024-07-05 08:55:11 +0100

    add specific package versions in requirements.txt. update pyproject.toml to install from requirements.txt

--- setup.py
+++
@@ -1,8 +0,0 @@
-import setuptools
-
-requirements = []
-with open("requirements.txt", "r") as fh:
-    for line in fh:
-        requirements.append(line.strip())
-
-setuptools.setup(install_requires=requirements)
